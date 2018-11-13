ec517065a86844d559dc26ab94dfa36c57af1a25
Josua96 <joshuaenrique96@gmail.com>
2018-11-13 15:29:30 -0600

    Correccion en datos de pruebas,removiedo caracteres especiales

--- AzureProjectDemo/src/app/app.component.ts
+++ AzureProjectDemo/src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AzureProjectDemo';
+
+  public file:any= null;
+  public lines:String[];
+
+  public remainingRequest:number=0;
+  public totalRequest:number=0;
+  public finishedRequest:number=0;
+
+
+  constructor(){
+
+  }
+
+  fileChanged(event):any{
+    console.log("archivo ha cambiado");
+    this.file= event.target.files[0];
+  }
+
+  readFileContent(){
+    if (this.file===null){
+      alert("Antes debe seleccinar un archivo con extension csv");
+      return;
+    }
+
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+
+      console.log("contenido del archivo");
+      console.log(fileReader.result);
+
+    }
+
+    fileReader.onloadend=()=>{
+
+      this.lines = (fileReader.result as String).split('\r\n');
+      console.log("lineas leídas");
+      console.log(this.lines);
+    }
+  
+    fileReader.readAsText(this.file,"UTF-8");
+
+  }
+
+ // makeRequest
+
 }
--- DataSet/generator.py
+++ DataSet/generator.py
@@ -1,23 +1,24 @@
 import csv
 
-csvName= 'dataResult';
-openMOde='w';
+csvName= 'dataResult'
+openMOde='w'
+encode='utf-8'
 
 #cada tarea se asocia a una descripción predeterminada
 tasks=[{"Pasear al perro": [0,1,2,3,4,5,6]},
        {"Pasear al gato": [0,1,2,3,4,5,6]},
-       {"Reparación del automovil":[7]},
-       {"Reparación de Toyota 2018 ":[7,10]},
-       {"Reparación de Bici 20":[9]},
+       {"Reparacion del automovil":[7]},
+       {"Reparacion de Toyota 2018 ":[7,10]},
+       {"Reparacion de Bici 20":[9]},
        {"Terminar tercer avance": [11,12,13,14,15]},
        {"Proyecto independiente": [11,12,13,14,15]},
-       {"Asistencia de investigación": [11,12,13,14,15]},
-       {"Evento médico privado": [16,17,18,19,20]},
-       {"Evento médico público": [16,17,18,19]},
-       {"Corte del césped casa Juan": [20,21,22]},
-       {"Corte del césped casa Carla": [20,21,22]},
-       {"Corte del césped empresa El Destino": [20,21,22]},
-       {"Corte del césped empresa El Alto": [20,21,22]},
+       {"Asistencia de indagatoria": [11,12,13,14,15]},
+       {"Evento medico privado": [16,17,18,19,20]},
+       {"Evento medico publico": [16,17,18,19]},
+       {"Corte del cesped casa Juan": [20,21,22]},
+       {"Corte del cesped casa Carla": [20,21,22]},
+       {"Corte del cesped empresa El Destino": [20,21,22]},
+       {"Corte del cesped empresa El Alto": [20,21,22]},
        {"Evento judicial": [23,24,25]}
     ]
 
@@ -27,25 +28,25 @@ descriptions=[
     "Paseo en el patio",
     "Paseo en la ciudad",
     "Paseo alrededor de la pradera",
-    "Paseo Ccn mis hijos",
+    "Paseo ccn mis hijos",
     "Paseo por la tarde",
     "Ir al centro de reparaciones Don Carlos",
     "Ir al centro de reparaciones Juan Miguel",
     "Ir al taller de motocicletas Montano",
     "Ir al centro oficial de la Toyota",
-    "Reunión inicial de grupo",
-    "Asesoría con el líder del trabajo",
-    "Reunión para recibir retroalimentación",
+    "Reunion inicial de grupo",
+    "Asesoria con el lider del trabajo",
+    "Reunion para recibir retroalimentacion",
     "Efectuar las correciones indicadas",
-    "Cita médica con el dentista",
+    "Cita medica con el dentista",
     "Cita para solicitar lentes ante la CCSS",
-    "Cita para revisión ocular de rutina",
-    "Operación del corazón",
-    "Transplante de riñon",
-    "Poda del corte clásico",
+    "Cita para revision ocular de rutina",
+    "Operacion del corazon",
+    "Transplante de higado",
+    "Poda del corte clasico",
     "Poda del corte especializado con escultura",
     "Poda del corte avanzado de figuras de animales",
-    "Juicio por evasión de impuestos",
+    "Juicio por evasion de impuestos",
     "Poner denuncia por intento de asesinato",
     "Juicio por encubrimiento de asesinato"
     ]
@@ -69,7 +70,7 @@ def generateCombinations(writer,tasks,descriptions,states):
 
 def generateCSVFile(tasks,descriptions,states):
 
-    with open ('dataResult.csv','w', newline='') as f:
+    with open ('dataResult.csv','w', newline='', encoding=encode) as f:
         writer= csv.writer(f)
         generateCombinations(writer, tasks,descriptions,states)
         return
