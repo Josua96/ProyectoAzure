fcc9ed10de0a261c542437ba006eebcbe23e7a93
Unknown <juliomontano008@gmail.com>
2018-11-14 22:40:26 -0600

    Actualización miércoles 14 de noviembre del 2018
    Se completo:
    1. Envío de tareas
    2. Carga de archivos

--- TaskManager/TaskManager/Form1.Designer.cs
+++ TaskManager/TaskManager/Form1.Designer.cs
@@ -28,66 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.botonEnviar = new System.Windows.Forms.Button();
             this.Ruta = new System.Windows.Forms.Label();
             this.FileDialog = new System.Windows.Forms.OpenFileDialog();
             this.AbrirButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.Total = new System.Windows.Forms.Label();
+            this.Atendidas = new System.Windows.Forms.Label();
+            this.NoAtendidas = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // botonEnviar
             // 
-            this.button1.Location = new System.Drawing.Point(99, 155);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Enviar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.botonEnviar.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonEnviar.Location = new System.Drawing.Point(230, 259);
+            this.botonEnviar.Name = "botonEnviar";
+            this.botonEnviar.Size = new System.Drawing.Size(75, 30);
+            this.botonEnviar.TabIndex = 0;
+            this.botonEnviar.Text = "Enviar";
+            this.botonEnviar.UseVisualStyleBackColor = true;
+            this.botonEnviar.Click += new System.EventHandler(this.botonEnviar_Click);
             // 
             // Ruta
             // 
             this.Ruta.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.Ruta.AutoSize = true;
-            this.Ruta.Location = new System.Drawing.Point(106, 101);
+            this.Ruta.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ruta.Location = new System.Drawing.Point(106, 98);
             this.Ruta.Name = "Ruta";
-            this.Ruta.Size = new System.Drawing.Size(81, 13);
+            this.Ruta.Size = new System.Drawing.Size(203, 19);
             this.Ruta.TabIndex = 1;
-            this.Ruta.Text = "Tareas a enviar";
+            this.Ruta.Text = "Ningun archivo seleccionado";
             // 
             // FileDialog
             // 
-            this.FileDialog.FileName = "openFileDialog1";
-            this.FileDialog.Filter = "\"Archivos csv\"|(*.csv)";
+            this.FileDialog.Filter = "Archivos de csv (*.csv)|*.csv";
             // 
             // AbrirButton
             // 
+            this.AbrirButton.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AbrirButton.Location = new System.Drawing.Point(25, 96);
             this.AbrirButton.Name = "AbrirButton";
-            this.AbrirButton.Size = new System.Drawing.Size(75, 23);
+            this.AbrirButton.Size = new System.Drawing.Size(75, 28);
             this.AbrirButton.TabIndex = 2;
             this.AbrirButton.Text = "Abrir archivo";
             this.AbrirButton.UseVisualStyleBackColor = true;
-            this.AbrirButton.Click += new System.EventHandler(this.button2_Click);
+            this.AbrirButton.Click += new System.EventHandler(this.AbrirButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(76, 52);
+            this.label2.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(134, 27);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.Size = new System.Drawing.Size(297, 26);
             this.label2.TabIndex = 3;
             this.label2.Text = "Seleccciona el archivo de tareas";
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Total.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Total.Location = new System.Drawing.Point(21, 149);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(170, 22);
+            this.Total.TabIndex = 4;
+            this.Total.Text = "Total de peticiones: 0";
+            // 
+            // Atendidas
+            // 
+            this.Atendidas.AutoSize = true;
+            this.Atendidas.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Atendidas.ForeColor = System.Drawing.Color.LimeGreen;
+            this.Atendidas.Location = new System.Drawing.Point(21, 184);
+            this.Atendidas.Name = "Atendidas";
+            this.Atendidas.Size = new System.Drawing.Size(184, 22);
+            this.Atendidas.TabIndex = 5;
+            this.Atendidas.Text = "Peticiones atendidas: 0";
+            // 
+            // NoAtendidas
+            // 
+            this.NoAtendidas.AutoSize = true;
+            this.NoAtendidas.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NoAtendidas.ForeColor = System.Drawing.Color.DarkRed;
+            this.NoAtendidas.Location = new System.Drawing.Point(21, 206);
+            this.NoAtendidas.Name = "NoAtendidas";
+            this.NoAtendidas.Size = new System.Drawing.Size(208, 22);
+            this.NoAtendidas.TabIndex = 6;
+            this.NoAtendidas.Text = "Peticiones no atendidas: 0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 272);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(693, 325);
+            this.Controls.Add(this.NoAtendidas);
+            this.Controls.Add(this.Atendidas);
+            this.Controls.Add(this.Total);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.AbrirButton);
             this.Controls.Add(this.Ruta);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.botonEnviar);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Task manager";
             this.ResumeLayout(false);
@@ -97,11 +141,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button botonEnviar;
         private System.Windows.Forms.Label Ruta;
         private System.Windows.Forms.OpenFileDialog FileDialog;
         private System.Windows.Forms.Button AbrirButton;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.Label Atendidas;
+        private System.Windows.Forms.Label NoAtendidas;
     }
 }
 
--- TaskManager/TaskManager/Form1.cs
+++ TaskManager/TaskManager/Form1.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,52 +11,111 @@ namespace TaskManager
 {
     public partial class Form1 : Form
     {
+        private List<string> listaTareas;
+        private int peticionesAtendidas = 0;
+        private int peticionesNoAtendidas = 0;
         public Form1()
         {
             InitializeComponent();
+            listaTareas = new List<string>();            
         }
 
 
-        private async Task hacerPeticion(string tarea)
+        private async Task HacerPeticion(string tarea)
         {
             string page = "https://apitaskso.azurewebsites.net/api/HttpTrigger1?code=NvAazVaa73a47rHdYTa3taI3VLj1j8uMW0IgO1DiMmkPWhfza78uBw==&message=" + tarea;
+
             using (HttpClient client = new HttpClient())
             {
                 using (HttpResponseMessage response = await client.GetAsync(page))
                 using (HttpContent content = response.Content)
                 {
+                    Console.WriteLine(content.ToString());
                     string result = await content.ReadAsStringAsync();
                     if (result != null)
                     {
+                        if(result.Contains("true"))
+                        {
+                            peticionesAtendidas++;
+                            Atendidas.Text = "Peticiones atendidas: " + peticionesAtendidas.ToString();
+                        }
+                        else if(result.Contains("false"))
+                        {
+                            peticionesNoAtendidas++;
+                            Atendidas.Text = "Peticiones atendidas: " + peticionesNoAtendidas.ToString();
+                        }
                         Console.WriteLine(result);
                     }
                 }
             }
         }
+     
+   
+     
+        private void LeerRegistrosCSV(string ruta)
+        {
+            listaTareas.Clear();
+            peticionesAtendidas = 0;
+            peticionesNoAtendidas = 0;
 
-        private void button1_Click_1(object sender, EventArgs e)
+            using (StreamReader sr = new StreamReader(ruta))
+            {
+                string currentLine;
+                while ((currentLine = sr.ReadLine()) != null)
+                {                                     
+                    listaTareas.Add(currentLine);
+                    Console.WriteLine(currentLine);
+                }
+            }
+
+            Total.Text = "Total de peticiones: " + listaTareas.Count;
+        }
+   
+
+        private async void  botonEnviar_Click(object sender, EventArgs e)
         {
 
-            string tarea = "Hacer investigación,Lo mas pronto posible,1";
-            hacerPeticion(tarea);
+           
+            foreach (string tarea in listaTareas)
+            {
+                await HacerPeticion(tarea);
+            }
         }
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
-        }
-
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if(FileDialog.ShowDialog()==DialogResult.OK)
+        private void AbrirButton_Click(object sender, EventArgs e)
+        {          
+            if (FileDialog.ShowDialog() == DialogResult.OK)
             {
                 Ruta.Text = FileDialog.FileName;
-            }
-            else
-            {
+                LeerRegistrosCSV(Ruta.Text);
+            }            
+        }
+    }
 
-            }
+    internal static class AsyncHelper
+    {
+        private static readonly TaskFactory _myTaskFactory = new
+          TaskFactory(CancellationToken.None,
+                      TaskCreationOptions.None,
+                      TaskContinuationOptions.None,
+                      TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return AsyncHelper._myTaskFactory
+              .StartNew<Task<TResult>>(func)
+              .Unwrap<TResult>()
+              .GetAwaiter()
+              .GetResult();
+        }
+
+        public static void RunSync(Func<Task> func)
+        {
+            AsyncHelper._myTaskFactory
+              .StartNew<Task>(func)
+              .Unwrap()
+              .GetAwaiter()
+              .GetResult();
         }
     }
 }
